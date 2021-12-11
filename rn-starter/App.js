ad041293bc245c575465545e5555fa7784ce0d26
Devrath Rana <devrath.rana@kelltontech.com>
2021-12-12 01:06:54 +0530

    fixes

--- rn-starter/App.js
+++ rn-starter/App.js
@@ -1,13 +1,16 @@
 import { createAppContainer } from "react-navigation";
 import { createStackNavigator } from "react-navigation-stack";
 import HomeScreen from "./src/screens/HomeScreen";
+import ComponentsScreen from './src/screens/ComponentsScreen';
 
+// Stack Navigator is a little object that decides what content we're going to show on the screen at any given point in time.So, we want to add in the component that we just created as an additional option for this STACK navigator to show.
 const navigator = createStackNavigator(
   {
     Home: HomeScreen,
+    Components:ComponentsScreen,//We added this new component as a possible option for our stack navigator to show to the user.
   },
   {
-    initialRouteName: "Home",
+    initialRouteName: "Components",//This is description or configuration option that tells this navigator thing what the first component is that should be displayed right now.Here it is set as Home so it will search the Home option from above options or stack and display that component screen first.
     defaultNavigationOptions: {
       title: "App",
     },
