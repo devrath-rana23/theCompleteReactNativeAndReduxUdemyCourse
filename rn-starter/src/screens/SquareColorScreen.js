223f40750b95090268b3f314998dfbe9ca7151dc
Devrath Rana <devrath.rana@kelltontech.com>
2021-12-17 00:33:36 +0530

    fixes

--- rn-starter/src/screens/SquareColorScreen.js
+++ rn-starter/src/screens/SquareColorScreen.js
@@ -16,12 +16,20 @@ const SquareColorScreen = () => {
         // color === 'red', 'green', 'blue'
         // change === '+10', '-10'
 
-        if (color === 'red') {
-            if ((red + change) <= 255 && (red + change) >= 0) setRed(red + change);
-        } else if (color === 'green') {
-            if ((green + change) <= 255 && (green + change) >= 0) setGreen(green + change);
-        } else if (color === 'blue') {
-            if ((blue + change) <= 255 && (blue + change) >= 0) setBlue(blue + change);
+        switch (color) {
+
+            case 'red':
+                (((red + change) <= 255) && ((red + change) >= 0)) ? setRed(red + change) : '';
+                return;//terminate with this each case so that it does not run other cases
+            case 'green':
+                (((green + change) <= 255) && ((green + change) >= 0)) ? setGreen(green + change) : '';
+                return;
+            case 'blue':
+                (((blue + change) <= 255) && ((blue + change) >= 0)) ? setBlue(blue + change) : '';
+                return;
+            default:
+                return;
+
         }
 
     };
@@ -34,19 +42,19 @@ const SquareColorScreen = () => {
                 style={styles.body}>
                 <ColorCounter
                     color='Red'
-                    onIncrease={() => setColor('red', +COLOR_COUNTER_CHANGE_VALUE)}
-                    onDecrease={() => setColor('red', -COLOR_COUNTER_CHANGE_VALUE)
+                    onIncrease={() => setColor('red', COLOR_COUNTER_CHANGE_VALUE)}
+                    onDecrease={() => setColor('red', -1 * COLOR_COUNTER_CHANGE_VALUE)
                     }
                 />
                 <ColorCounter
                     color='Green'
-                    onIncrease={() => setColor('green', +COLOR_COUNTER_CHANGE_VALUE)}
-                    onDecrease={() => setColor('green', -COLOR_COUNTER_CHANGE_VALUE)}
+                    onIncrease={() => setColor('green', COLOR_COUNTER_CHANGE_VALUE)}
+                    onDecrease={() => setColor('green', -1 * COLOR_COUNTER_CHANGE_VALUE)}
                 />
                 <ColorCounter
                     color='Blue'
-                    onIncrease={() => setColor('blue', +COLOR_COUNTER_CHANGE_VALUE)}
-                    onDecrease={() => setColor('blue', -COLOR_COUNTER_CHANGE_VALUE)}
+                    onIncrease={() => setColor('blue', COLOR_COUNTER_CHANGE_VALUE)}
+                    onDecrease={() => setColor('blue', -1 * COLOR_COUNTER_CHANGE_VALUE)}
                 />
                 <View style={{ height: 100, width: 100, backgroundColor: `rgb(${red},${green},${blue})` }} //double {{}} means outer set reference to some javascript expression and inner set creates an object literal 
                 />
