53de4d2731d4a2ba999c4968df7bd3ee9e143f39
Devrath Rana <devrath.rana@kelltontech.com>
2021-12-16 23:49:56 +0530

    fixes

--- rn-starter/src/screens/SquareColorScreen.js
+++ rn-starter/src/screens/SquareColorScreen.js
@@ -1,93 +1,65 @@
 import React, { useState } from 'react';
-import { View, StyleSheet, Text, TouchableOpacity } from 'react-native';
+import { View, StyleSheet } from 'react-native';
 import { SafeAreaProvider } from 'react-native-safe-area-context';
+import ColorCounter from '../components/ColorCounter';
 
 const SquareColorScreen = () => {
 
-    const [red, setRed] = useState(255);
-    const [green, setGreen] = useState(255);
-    const [blue, setBlue] = useState(255);
+    const [red, setRed] = useState(0);
+    const [green, setGreen] = useState(0);
+    const [blue, setBlue] = useState(0);
 
-    const showColor = (colorSelected) => {
-
-        if (red <= 255 && red >= 0 && green <= 255 && green >= 0 && blue <= 255 && blue >= 0) {
-            if (colorSelected === 'moreRed') {
-                if (red !== 255) {
-                    let newRed = red + 1;
-                    setRed(newRed);
-                }
-            } else if (colorSelected === 'lessRed') {
-                if (red !== 0) {
-                    let newRed = red - 1;
-                    setRed(newRed);
-                }
-            } else if (colorSelected === 'moreGreen') {
-                if (green !== 255) {
-                    let newGreen = green + 1;
-                    setGreen(newGreen);
-                }
-            } else if (colorSelected === 'lessGreen') {
-                if (green !== 0) {
-                    let newGreen = green - 1;
-                    setGreen(newGreen);
-                }
-            } else if (colorSelected === 'moreBlue') {
-                if (blue !== 255) {
-                    let newBlue = blue + 1;
-                    setBlue(newBlue);
-                }
-            } else if (colorSelected === 'lessBlue') {
-                if (blue !== 0) {
-                    let newBlue = blue - 1;
-                    setBlue(newBlue);
-                }
-            }
-        }
-        console.log(`Red-${red},Green-${green},Blue-${blue}`);
-    }
+    console.log(`Red-${red},Green-${green},Blue-${blue}`);
 
     return (
         <SafeAreaProvider>
-            <View>
-                <View>
-                    <Text>Red</Text>
-                    <TouchableOpacity
-                        onPress={() => showColor('moreRed')}
-                    >
-                        <Text>More Red</Text>
-                    </TouchableOpacity>
-                    <TouchableOpacity
-                        onPress={() => showColor('lessRed')}
-                    >
-                        <Text>Less Red</Text>
-                    </TouchableOpacity>
-                </View>
-                <View>
-                    <Text>Green</Text>
-                    <TouchableOpacity
-                        onPress={() => showColor('moreGreen')}
-                    >
-                        <Text>More Green</Text>
-                    </TouchableOpacity>
-                    <TouchableOpacity
-                        onPress={() => showColor('lessGreen')}
-                    >
-                        <Text>Less Green</Text>
-                    </TouchableOpacity>
-                </View>
-                <View>
-                    <Text>Blue</Text>
-                    <TouchableOpacity
-                        onPress={() => showColor('moreBlue')}
-                    >
-                        <Text>More Blue</Text>
-                    </TouchableOpacity>
-                    <TouchableOpacity
-                        onPress={() => showColor('lessBlue')}
-                    >
-                        <Text>Less Blue</Text>
-                    </TouchableOpacity>
-                </View>
+            <View
+                style={styles.body}>
+                <ColorCounter
+                    color='Red'
+                    onIncrease={() => {
+                        if (red <= 255 && red >= 0 && red !== 255) {
+                            setRed(red + 1);
+                        }
+                    }
+                    }
+                    onDecrease={() => {
+                        if (red <= 255 && red >= 0 && red !== 0) {
+                            setRed(red - 1);
+                        }
+                    }
+                    }
+                />
+                <ColorCounter
+                    color='Green'
+                    onIncrease={() => {
+                        if (green <= 255 && green >= 0 && green !== 255) {
+                            setGreen(green + 1);
+                        }
+                    }
+                    }
+                    onDecrease={() => {
+                        if (green <= 255 && green >= 0 && green !== 0) {
+                            setGreen(green - 1);
+                        }
+                    }
+                    }
+                />
+                <ColorCounter
+                    color='Blue'
+                    onIncrease={() => {
+                        if (blue <= 255 && blue >= 0 && blue !== 255) {
+                            setBlue(blue + 1);
+                        }
+                    }
+                    }
+                    onDecrease={() => {
+                        if (blue <= 255 && blue >= 0 && blue !== 0) {
+                            setBlue(blue - 1);
+                        }
+                    }
+                    }
+                />
                 <View style={{ height: 100, width: 100, backgroundColor: `rgb(${red},${green},${blue})` }} />
             </View>
         </SafeAreaProvider>
@@ -96,7 +68,9 @@ const SquareColorScreen = () => {
 };
 
 const styles = StyleSheet.create({
-
+    body: {
+        flex: 1,
+    },
 });
 
 export default SquareColorScreen;
