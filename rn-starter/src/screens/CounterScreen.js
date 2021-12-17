40f8a0e69c55d1b165c655998d438f8628ea0ccc
Devrath Rana <devrath.rana@kelltontech.com>
2021-12-17 18:14:36 +0530

    Reducers

--- rn-starter/src/screens/CounterScreen.js
+++ rn-starter/src/screens/CounterScreen.js
@@ -1,63 +1,48 @@
-import React, { useState } from 'react';//useState is a hook i.e. a function that adds some new functionality to a function component.
+import React, { useReducer } from 'react';
 import { View, Text, StyleSheet, TouchableOpacity } from 'react-native';
 import { SafeAreaProvider } from 'react-native-safe-area-context';
 
+const COUNTER_INCREMENT = 1;
+
+const reducer = (state, action) => {
+    //state==={counter:0}
+    //action==={type:'change_counter', payload:1||-1}
+
+    console.log(action);
+    switch (action.type) {
+
+        case 'change_counter':
+            return state.counter + action.payload < 0
+                ?
+                state
+                :
+                { ...state, counter: state.counter + action.payload };
+
+        default:
+            return state;
+
+    }
+
+};
+
 const CounterScreen = () => {
 
-    //In react native app if you instead of updating state variable simply updates a normal variable which is not a state, that update will not be highlighted even if it's value is updated which you can see by console.log(updated variable)
+    const [state, dispatch] = useReducer(reducer, { counter: 0 });
 
-    /* const [counterValue, setCounterValue] is referred to as array destructuring. Another way of pulling or referencing some elements out of the array.
-
-    Eg:
-    const colors = ['red','green'];
-
-    Two ways to access colors array values=>
-    
-    WAY ONE
-    colors[0];
-    colors[1];
-    
-    WAY TWO
-    const ['colorOne', 'colorTwo'] = colors;
-
-    So we want JS to take first value of colors array and assign it to colorOne variable
-    and take second value of colors array and assign it to colorTwo variable
-
-    colorOne// O/P=>red
-    colorTwo// O/P=>green
-
-    This syntax helps us to assign whatever useState is returning to us simpler
-
-    NOTE: Every copy or instance of a component used inside this functional component will have it's own pieces of state
-    */
-
-    const [counterValue, setCounterValue] = useState(0);//To change state variable we use second thing that comes to us when we call useState() => setCounterValue function. Whenever this functio is called react-native automatically reruns the function component we have created.
-
-    const increaseCounter = () => {
-        let reqCounterValue = counterValue + 1;
-        setCounterValue(reqCounterValue);
-        console.log(counterValue);//not shows updated value as it takes some time to update and re-run this function component
-    };
-
-    const decreaseCounter = () => {
-        let reqCounterValue = counterValue - 1;
-        if (reqCounterValue >= 0) {
-            setCounterValue(reqCounterValue);
-        }
-    };
+    console.log(state);
 
     return (
         <SafeAreaProvider>
             <View style={styles.body}>
                 <TouchableOpacity
                     style={styles.button}
-                    onPress={increaseCounter}
+                    onPress={() => dispatch({ type: 'change_counter', payload: COUNTER_INCREMENT })}
                 >
                     <Text style={styles.buttonTitle}>Increase</Text>
                 </TouchableOpacity>
                 <TouchableOpacity
                     style={styles.button}
-                    onPress={decreaseCounter}
+                    onPress={() => dispatch({ type: 'change_counter', payload: -1 * COUNTER_INCREMENT })}
                 >
                     <Text style={styles.buttonTitle}>Decrease</Text>
                 </TouchableOpacity>
@@ -65,7 +50,7 @@ const CounterScreen = () => {
                     Current Count:
                 </Text>
                 <Text style={styles.counterText}>
-                    {counterValue}
+                    {state.counter}
                 </Text>
             </View>
         </SafeAreaProvider>
